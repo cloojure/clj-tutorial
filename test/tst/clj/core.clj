c972014767bc61e2c117403f0a8782d2a2c17347
Alan Thompson <cloojure@gmail.com>
2017-05-17 23:21:16 -0700

    misc

--- test/tst/clj/core.clj
+++ test/tst/clj/core.clj
@@ -11,7 +11,16 @@
 (t/print-versions)
 
 (dotest
-  (is= 5 (+ 2 3)))
+  (is= 5 (+ 2 3))
+
+  (let [iiinc-txt    "(fn fn-iiinc [x] (+ 3 x))"
+        fn-iiinc-str (eval (read-string iiinc-txt))]
+    (is= 5 (fn-iiinc-str 2)))
+
+  (let [iiinc-ast    '(fn fn-iiinc [x] (+ 3 x))
+        fn-iiinc-ast (eval iiinc-ast)]
+    (is= 5 (fn-iiinc-ast 2)))
+)
 
 
 
